7ed5cf5d482b3fb80fc5ac1f57acf2d6ca7e74d3
Neha_headerlabs <neha@headerlabs.com>
2018-04-05 09:14:05 +0530

    Initial Commit

---
+++ WVFramework/WVFramework.h
@@ -0,0 +1,19 @@
+//
+//  WVFramework.h
+//  WVFramework
+//
+//  Created by Neha on 4/5/18.
+//  Copyright © 2018 Neha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WVFramework.
+FOUNDATION_EXPORT double WVFrameworkVersionNumber;
+
+//! Project version string for WVFramework.
+FOUNDATION_EXPORT const unsigned char WVFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WVFramework/PublicHeader.h>
+
+
